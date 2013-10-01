3507c12cd794dbaa93348024ca1524ed89752ea1
Giannis Chionidis <gc@inlead.dk>
2013-10-01 11:38:46 +0200

    case 3566 - Refactor css and fixing js popup issue on ie7 and ie8.

--- js/artesis_facebookshare.js
+++ js/artesis_facebookshare.js
@@ -8,34 +8,34 @@ function artesis_facebookshare_popup() {
   var head_meta_title = jQuery('meta[property="og:title"]').attr("content");
   var head_meta_description = jQuery('meta[property="og:description"]').attr("content");
   var head_meta_image = jQuery('meta[property="og:image"]').attr("content");
-  if(typeof head_meta_url != 'undefined') {
+  if(typeof head_meta_url !== 'undefined') {
     head_meta_url = '&p[url]=' + encodeURIComponent(head_meta_url);
   }
   else {
    head_meta_url = '';
   }
-  if(typeof head_meta_title != 'undefined') {
+  if(typeof head_meta_title !== 'undefined') {
     head_meta_title = '&p[title]=' + encodeURIComponent(head_meta_title);
   }
   else {
    head_meta_title = '';
   }
-  if(typeof head_meta_description != 'undefined') {
+  if(typeof head_meta_description !== 'undefined') {
     head_meta_description = '&p[summary]=' + encodeURIComponent(head_meta_description);
   }
   else {
    head_meta_description = '';
   }
-  if(typeof head_meta_image != 'undefined') {
+  if(typeof head_meta_image !== 'undefined') {
     head_meta_image = '&p[images][0]=' + encodeURIComponent(head_meta_image);
   }
   else {
    head_meta_image = '';
   }
   window.open(
-    'https://www.facebook.com/sharer/sharer.php?s=100' + head_meta_url + head_meta_title + head_meta_description + head_meta_image,
-    'facebook-share-dialog', 
-    'width=626,height=436'); 
+    "https://www.facebook.com/sharer/sharer.php?s=100" + head_meta_url + head_meta_title + head_meta_description + head_meta_image,
+    "facebook", 
+    "width=626,height=436");
     return false;
 }
 Drupal.behaviors.artesis_facebookshare = {
