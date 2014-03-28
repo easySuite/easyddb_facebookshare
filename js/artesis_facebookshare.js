e1728a005ea9eb3ebae9b314c75f9bc0f351248d
George Mihailov <gm@inlead.dk>
2014-03-28 13:30:54 +0200

    case 3959 - Added Drupal block to increase possible ways of adding FB plugin.

--- js/artesis_facebookshare.js
+++ js/artesis_facebookshare.js
@@ -4,11 +4,13 @@
 
 Drupal.behaviors.artesis_facebookshare = {
   attach: function (settings) {
-    jQuery('.artesis_facebookshare').once('fb').facebook("likebutton", {
-      url: window.location.href,
-      layout: Drupal.settings.artesis_facebookshare.layout,
-      share: Drupal.settings.artesis_facebookshare.share,
-      action: Drupal.settings.artesis_facebookshare.action
+    jQuery('.artesis_facebookshare').once('fb').each(function() {
+      jQuery(this).facebook("likebutton", {
+        url: window.location.href,
+        layout: Drupal.settings.artesis_facebookshare[this.id].layout,
+        share: Drupal.settings.artesis_facebookshare[this.id].share,
+        action: Drupal.settings.artesis_facebookshare[this.id].action
+      });
     });
   }
 };
